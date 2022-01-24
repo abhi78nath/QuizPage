0ab22386c0bde06ad3bf26804cd2a1b3b9044149
abhishek nath <abhishek78nath@gmail.com>
2022-01-24 23:30:10 +0530

    first

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", function(req, res){
+    res.render('index');
+});
+
+app.get('/page1', function(req, res){
+    res.render("page1");
+})
+
+app.get('/a', function(req, res){
+    res.render('page2');
+});
+
+app.get('/b', function(req, res){
+    res.render('page2')
+});
+
+app.get("*",function(req,res){
+    res.render('error')
+})
+app.listen(port, function(){
+    console.log("De open hoise de......");
+});
